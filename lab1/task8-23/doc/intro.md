ea28d36007ed2abeb0d185f1c3c28a768011f150
Roman <romankrohin126@gmail.com>
2024-09-23 17:21:09 +0300

    chore: lein remove

--- lab1/task8-23/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to task8-23
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- lab1/task8-23/project.clj
+++
@@ -1,6 +0,0 @@
-(defproject my-app "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.3"]]
-  :plugins [[lein-cljfmt "0.6.8"]
-            [com.github.clj-kondo/lein-clj-kondo "0.2.5"]]
-  :aliases {"lint" ["clj-kondo" "--lint" "src" "test"]
-            "format" ["cljfmt" "fix"]})
